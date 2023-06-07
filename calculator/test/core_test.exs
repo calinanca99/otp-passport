3a83444e978d7eb27d853ac82394da0d4cf57f14
calinanca99 <calinanca99@gmail.com>
2023-06-07 07:16:48 +0200

    Define `Calculator.Core` module

---
+++ calculator/test/core_test.exs
@@ -0,0 +1,24 @@
+defmodule CoreTest do
+  use ExUnit.Case
+  import Calculator.Core
+
+  test "adds" do
+    assert add(2, 5) == 7
+  end
+
+  test "subtracts" do
+    assert subtract(10, 4) == 6
+  end
+
+  test "multiply" do
+    assert multiply(5, 2) == 10
+  end
+
+  test "divide" do
+    assert divide(12, 3) == 4
+  end
+
+  test "fold" do
+    assert fold([1, 2, 3, 4], 0, &add/2) == 10
+  end
+end
